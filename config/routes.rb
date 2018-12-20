6007216b4f3ec99144953055007b8685897468c8
Lucas Moore <hey@thelucasmoore.com>
2018-12-19 22:20:49 -0700

    generate some modesl

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'bouldering' => 'grades#bouldering'
+  get 'sport' => 'grades#sport'
+  resources :climbs
+  resources :grades
+  resources :routes
+  root 'application#hello'
 end
