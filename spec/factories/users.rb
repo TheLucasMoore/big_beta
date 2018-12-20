3bdc0dbeaa1b2d0037a4b52a4760d9d5c4ecaab8
Lucas Moore <hey@thelucasmoore.com>
2018-12-20 00:20:21 -0700

    writing integration test for login/signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Random.new(42) }
   end
 end
---
+++ spec/integration/user_registration_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /signup', type: :request do
+  let(:url) { 'users/sign_up' }
+  let(:user) { create(:user) }
+  let(:params) do
+    {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+  end
+
+  context 'when user is unauthenticated' do
+    before { post url, params: params }
+
+    it 'returns 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns a new user' do
+      expect(response.body).to match_schema('user')
+    end
+  end
+
+  context 'when user already exists' do
+    before do
+      post url, params: params
+    end
+
+    it 'returns bad request status' do
+      expect(response.status).to eq 400
+    end
+
+    it 'returns validation errors' do
+      expect(json['errors'].first['title']).to eq('Bad Request')
+    end
+  end
+end
